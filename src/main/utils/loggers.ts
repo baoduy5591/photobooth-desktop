892fd52ef5dfecb33754b0dfbe0a70d7365d50b1
codeismylife <codeismylife@codeismylifes-MacBook-Pro.local>
2024-08-16 11:34:15 +0700

    config loggers

---
+++ src/main/utils/loggers.ts
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import winston from 'winston';
+import Paths from './paths';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const {combine, timestamp, json, prettyPrint, errors } = winston.format;
+
+
+class Loggers {
+  isShowConsole: boolean;
+  pathMainLogger: string;
+  pathRendererLogger: string;
+  mainId: string;
+  rendererId: string;
+
+  constructor(isShowConsole = true) {
+    this.isShowConsole = isShowConsole;
+    this.mainId = 'MainProcess';
+    this.rendererId = 'RendererProcess';
+    this.pathMainLogger = this.getFolderLoggers() + '/main-%DATE%.log';
+    this.pathRendererLogger = this.getFolderLoggers() + '/renderer-%DATE%.log';
+    this.registryLoggers();
+  }
+
+  getFolderLoggers() {
+    const path = Paths.getFolderAppData() + "/loggers";
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+
+    return path;
+  }
+  
+  configsDailyRotateTransport(pathFile: string) {
+    return new DailyRotateFile({
+      filename: pathFile,
+      datePattern: 'YYYY-MM-DD',
+      maxFiles: '30d',
+    })
+  }
+  
+  configsTransport(pathFile: string) {
+    if (this.isShowConsole) {
+      return [
+        new winston.transports.Console(),
+        this.configsDailyRotateTransport(pathFile)
+      ]
+    }
+
+    return [
+      this.configsDailyRotateTransport(pathFile)
+    ]
+  }
+
+  configs(server: string, pathFile: string) {
+    return {
+      level: 'info',
+      format: combine(
+        errors({ stack: true }),
+        timestamp(),
+        json(),
+        prettyPrint()
+      ),
+      transports: this.configsTransport(pathFile),
+      defaultMeta: {server: server}
+    }
+  }
+
+  registryLoggers() {
+    winston.loggers.add(this.mainId, this.configs(this.mainId, this.pathMainLogger));
+    winston.loggers.add(this.rendererId, this.configs(this.rendererId, this.pathRendererLogger));
+  }
+
+  getLoggers() {
+    const mainLogger = winston.loggers.get(this.mainId);
+    const rendererLogger = winston.loggers.get(this.rendererId);
+    return {
+      mainLogger,
+      rendererLogger
+    }
+  }
+}
+
+export default Loggers;
+
+
