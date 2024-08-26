f19c1f8d36239f19d13a8238ffb077cfb0988ab4
codeismylife <codeismylife@codeismylifes-MacBook-Pro.local>
2024-08-26 16:29:01 +0700

    configs copyWebpackPlugin for dev and prod

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,8 +1,22 @@
+// eslint-disable-next-line import/default
+import CopyPlugin from 'copy-webpack-plugin';
+
 import type { Configuration } from 'webpack';
 
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 import path from 'path';
+
+const env = process.env.NODE_ENV;
+
+const copyWebpackPlugin = env === 'development' ? new CopyPlugin({
+  patterns: [
+    {
+      from: path.join(__dirname, 'src', 'renderer', 'assets'),
+      to: 'assets/'
+    }
+  ]
+}) : null;
 
 rules.push({
   test: /\.css$/,
@@ -13,11 +27,15 @@ export const rendererConfig: Configuration = {
   module: {
     rules,
   },
-  plugins,
+  plugins: [
+    ...plugins,
+    copyWebpackPlugin
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src/')
     },
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
+  
 };
