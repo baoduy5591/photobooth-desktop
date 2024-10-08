6464dd48ba145b4f33cd97eb8a8758d821baaae0
lekhabaoduy <baoduy5591@gmail.com>
2024-10-08 11:16:18 +0700

    fix ratio

--- src/renderer/pages/shooting.tsx
+++ src/renderer/pages/shooting.tsx
@@ -204,16 +204,16 @@ export default function Shooting() {
                 <DisplayImage src={store.pathFolderAssets + store.resources.icons[32]?.relPath} />
               </div>
 
-              {shootingPhotos.length > 0 && (
-                <div className='absolute inset-0 flex justify-center p-4'>
-                  <div
-                    className='h-[220px] rounded-xl border-2 border-custom-style-2-1'
-                    style={{ width: `${220 * store.orderInfo.ratio}px` }}
-                  >
+              <div className='absolute inset-0 flex justify-center p-4'>
+                <div
+                  className='h-[220px] rounded-xl border-2 border-custom-style-2-1'
+                  style={{ width: `${220 * store.orderInfo.ratio}px` }}
+                >
+                  {shootingPhotos && (
                     <DisplayImage src={store.pathFolderUserPhotos + '/' + shootingPhotos.slice(-1)[0]} />
-                  </div>
+                  )}
                 </div>
-              )}
+              </div>
             </div>
           </div>
         </div>
