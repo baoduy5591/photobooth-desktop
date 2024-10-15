2be3dd200051d67c314c1739475b538f80f2af91
lekhabaoduy <baoduy5591@gmail.com>
2024-10-15 14:59:06 +0700

    fix rotation

--- src/renderer/pages/selectSticker.tsx
+++ src/renderer/pages/selectSticker.tsx
@@ -166,7 +166,7 @@ export default function SelectSticker() {
     const deltaX = pageX - selectedSticker[index].currentPageX;
     const deltaY = pageY - selectedSticker[index].currentPageY;
     console.log(deltaX, deltaY);
-    if (Math.abs(deltaX) < 25 || Math.abs(deltaY) < 25) return;
+    if (Math.abs(deltaX) < 25 && Math.abs(deltaY) < 25) return;
 
     const frameRect = frameRef.current.getBoundingClientRect();
     if (!frameRect) return;
