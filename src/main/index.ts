0d7203c3539283dec05d0defcda2d2c8c2d6c03d
lekhabaoduy <baoduy5591@gmail.com>
2024-12-23 16:29:43 +0700

    refactor code

--- src/main/index.ts
+++ src/main/index.ts
@@ -33,7 +33,7 @@ const createWindow = (): void => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 // ipcMain get resources
