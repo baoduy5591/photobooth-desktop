18180d70b5d488cae2a77c53bea9cea9179f36fa
lekhabaoduy <baoduy5591@gmail.com>
2024-12-23 16:22:18 +0700

    refactor code

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -42,8 +42,8 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* <Route path='/' element={<Splash />} /> */}
-        <Route path='/' element={<SelectPhotos />} />
+        <Route path='/' element={<Splash />} />
+        {/* <Route path='/' element={<SelectPhotos />} /> */}
         <Route path='/home' element={<Home />} />
         <Route path='/enter-code' element={<EnterCode />} />
         <Route path='/shooting' element={<Shooting />} />
